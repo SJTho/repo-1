4276f0ed4ec840b18dcd3f564be3f8805fb54ba0
SJTho <SJTho@aol.com>
2024-07-29 17:23:32 +0100

    update

--- Surgical_Training/Script1.js
+++ Surgical_Training/Script1.js
@@ -5,14 +5,15 @@
 //display lists by user type
 
 
+
 function updatelinks(role) {
 
+    var ele = document.getElementById("linklist").children;
+    for (let i = 0; i < ele.length; i++) {
+        if (ele[i].className.substr(ele[i].length -6) != "hidden") {ele[i].classList.add("hidden") };
+        if (ele[i].className == role + " hidden") { ele[i].classList.remove("hidden") }; 
 
-    alert(role);
-
-    let elements = document.getElementsByClassName(role);
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.add("hidden");
+            
     }
-  
+
 }
