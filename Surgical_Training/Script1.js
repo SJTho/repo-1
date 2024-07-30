c27cbf5eb8dbbaa92311b63646c8130d78adb1db
SJTho <SJTho@aol.com>
2024-07-30 07:10:41 +0100

    update

--- Surgical_Training/Script1.js
+++ Surgical_Training/Script1.js
@@ -9,7 +9,7 @@
 function updatelinks(role) {
 
     var ele = document.getElementById("linklist").children;
-    for (let i = 0; i < ele.length; i++) {
+    for (var i = 0; i < ele.length; i++) {
         if (ele[i].className.substr(ele[i].length -6) != "hidden") {ele[i].classList.add("hidden") };
         if (ele[i].className == role + " hidden") { ele[i].classList.remove("hidden") }; 
 
