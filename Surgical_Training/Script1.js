7cd62c444eb5a0b4847eaff876d4ce850ef39167
SJTho <SJTho@aol.com>
2024-08-12 16:12:46 +0100

    update

--- Surgical_Training/Script1.js
+++ Surgical_Training/Script1.js
@@ -24,10 +24,7 @@
     })
 
 
- // This hides the address bar:
-document.addEventListener("click", function () {
-    window.scrollTo(0, 1)
-});
+
 
 //define the start array if not in localstorage
 
