91ba976393372fcaf4e3ecc87c169d186b71555f
SJTho <SJTho@aol.com>
2026-08-06 15:39:27 +0100

    Add files via upload

---
+++ surgilinks2026/TestDates.js
@@ -0,0 +1,30 @@
+const TestDates = [
+  {
+    title: "Formative Annual Examination",
+    date: "2026-09-12",
+    specialty: "Neurosurgery",
+    startTime: "09:00",
+    finishTime: "12:00"
+  },
+  {
+    title: "Formative Annual Examination",
+    date: "2026-10-01",
+    specialty: "Trauma and Orthopaedics",
+    startTime: "13:00",
+    finishTime: "17:00"
+  },
+  {
+    title: "Formative Annual Examination",
+    date: "2026-08-20",
+    specialty: "Urology",
+    startTime: "10:00",
+    finishTime: "14:00"
+  },
+  {
+    title: "Formative Annual Examination",
+    date: "2026-09-05",
+    specialty: "General Surgery",
+    startTime: "08:30",
+    finishTime: "11:30"
+  }
+];
