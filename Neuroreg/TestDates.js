405d2a9604fe6f33a834d5fc9c7d948fb0290059
SJTho <SJTho@aol.com>
2026-08-09 18:38:01 +0100

    Add files via upload

---
+++ Neuroreg/TestDates.js
@@ -0,0 +1,33 @@
+window.TestDates = [
+  {
+    title: "Formative Annual Exam 2026",
+    date: "2026-08-10",
+    startTime: "07:00",
+    finishTime: "20:00"
+  },
+  {
+    title: "Formative Annual Exam 2025",
+    date: "2025-08-07",
+    startTime: "07:00",
+    finishTime: "20:00"
+  },
+  {
+    title: "Formative Annual Exam 2024",
+    date: "2024-08-07",
+    startTime: "07:00",
+    finishTime: "20:00"
+  },
+  {
+    title: "Formative Annual Exam 2023",
+    date: "2023-08-07",
+    startTime: "07:00",
+    finishTime: "20:00"
+  },
+  {
+    title: "Formative Annual Exam 2022",
+    date: "2022-08-07",
+    startTime: "07:00",
+    finishTime: "20:00"
+  },
+
+];
