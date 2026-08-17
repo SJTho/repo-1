21a350c82e3d46d2c384d040c94561a263a35ecb
SJTho <SJTho@aol.com>
2026-08-17 12:00:27 +0100

    Add files via upload

---
+++ Neuroreg/myenv.js
@@ -0,0 +1,4 @@
+// myenv.js (placed one level above login.html)
+
+export const SUPABASE_URL = "https://jzdvtqkssamxndgrttct.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6ZHZ0cWtzc2FteG5kZ3J0dGN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNDQ1NzAsImV4cCI6MjEwMTkyMDU3MH0.CHKV56rf8ZSwwKIN0fuzEBuPoxn8qwSW0YzW_1DFcgM";
