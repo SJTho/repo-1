264357e6b32116a40757e5a5372b438f03f3e717
SJTho <SJTho@aol.com>
2026-09-15 17:04:54 +0100

    Add files via upload

--- Neuroreg/scripts/operatingtheatre.js
+++ Neuroreg/scripts/operatingtheatre.js
@@ -1126,3 +1126,4 @@ function applyResponsiveLayout() {
 ---------------------------------------------------- */
 function dispatchTheatreChanged() {
     document.dispatchEvent(new CustomEvent("theatreChanged"));
+}
